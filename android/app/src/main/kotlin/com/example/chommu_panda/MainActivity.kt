b74bf4cfa2c8024fd12cb7d50fc95b536be98881
nehElephantE <nehelephante@gmail.com>
2023-08-09 18:58:28 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/chommu_panda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chommu_panda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Components/rounded_password_field.dart
@@ -0,0 +1,35 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:chommu_panda/Components/text_field_container.dart';
+import 'package:chommu_panda/constants.dart';
+
+class RoundedPasswordField extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+  const RoundedPasswordField({
+    Key key,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        obscureText: true,
+        onChanged: onChanged,
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          hintText: "Password",
+          icon: Icon(
+            Icons.lock,
+            color: kPrimaryColor,
+          ),
+          suffixIcon: Icon(
+            Icons.visibility,
+            color: kPrimaryColor,
+          ),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
